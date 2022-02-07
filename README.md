e2f818f48b1615e4b608f48651f3b5c0ba627b2f
ZachHofmeister <zachhof@me.com>
2022-02-07 11:46:09 -0800

    First commit and finished algorithms.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Roman Numeral Converter
+
+A couple Python algorithms that make use of dictionaries to convert between Roman Numeral strings and decimal numbers.
---
+++ main.py
@@ -0,0 +1,12 @@
+from roman_arabic_skeleton_code import *
+
+
+def main():
+    run_to_roman_to_arabic_tests(1, 4000, 37)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
